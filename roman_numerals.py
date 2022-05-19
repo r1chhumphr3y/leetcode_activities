27492a20a41bf1d2650c96913c082d63e82e29ff
Rich Humphrey <richhumphreyapps@gmail.com>
2022-05-19 08:20:10 -0600

    Initial commit.  Converts roman numeral string to integer.

---
+++ roman_numerals.py
@@ -0,0 +1,39 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        # create constants:
+        roman = { "I": 1,
+                "V": 5,
+                "X": 10,
+                "L": 50,
+                "C": 100,
+                "D": 500,
+                "M": 1000
+                }
+
+        int_value = 0
+
+        # for loop to iterate over words array
+        previous = 1001
+        for char in s:
+            # deubg: print(char)
+            if char in roman:
+                char_value = roman[char]
+                if (char_value <= previous):
+                    int_value += char_value
+                else:
+                    int_value = (int_value - previous) + (char_value - previous)
+
+                previous = char_value
+            else:
+                print("Error, invalid character")
+
+        return(int_value)
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    str = 'VI'
+    print("Converting: ", str)
+    solution = Solution;
+    print(solution.romanToInt(solution, str))
+
+
